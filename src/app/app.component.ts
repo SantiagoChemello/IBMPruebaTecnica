1c58230ace8cb9b1689887832851405e59a8e116
SantiagoChemello <chemello.santiago8@gmail.com>
2024-10-11 16:48:01 -0300

    Proyecto angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FilesComponent } from './files/files.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FilesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
